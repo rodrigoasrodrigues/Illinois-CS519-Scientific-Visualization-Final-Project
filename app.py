0157a5e06e05edc3378dea63e25ce9b569aafd66
Rodrigo Rodrigues <rodrigo@rodrigorodrigues.info>
2021-12-04 15:18:26 -0300

    setup heroku

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import pandas as pd
 
 app = dash.Dash(__name__)
+server = app.server
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
