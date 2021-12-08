f27ffd299480c25d029a331ba3589f5dd900e454
Evan <pickett7474@hotmail.com>
2021-12-07 22:39:37 -0800

    Use hardcoded data (for now)

--- layout/match_placement.py
+++ layout/match_placement.py
@@ -6,44 +6,64 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
 import pandas as pd
+import numpy as np
 import dash_bootstrap_components as dbc
 
 def match_placement_view():
+    matchName = '19751219-M-Davis_Cup_World_Group_F-RR-Bjorn_Borg-Jiri_Hrebec'
+    file = "charting-m-stats-ServeDirection.csv"
+    file_plus_path = "data/" + file
+    df = pd.read_csv(file_plus_path,names=['match_id','row','deuce_wide','deuce_middle','deuce_t','ad_wide','ad_middle','ad_t','err_net','err_wide','err_deep','err_wide_deep','err_foot','err_unknown'])
+    df = df.loc[(df['match_id']==matchName) & (df['row'] == '1 Total')]
+    df = pd.DataFrame(df,columns=['deuce_wide','deuce_middle','deuce_t','ad_wide','ad_middle','ad_t'])
+    location_map = {
+        "deuce_wide": [.8,.2], #todo: accuracy
+        "deuce_middle": [.8,.5], #todo: accuracy
+        "deuce_t": [.8,.8], #todo: accuracy
+        "ad_wide": [.2,.2], #todo: accuracy
+        "ad_middle": [.2,.5], #todo: accuracy
+        "ad_t": [.2,.8], #todo: accuracy
+    }
+    #https://tennishold.com/wp-content/uploads/2018/09/Deuce-Tennis-Court-Diagram.jpg
+    npIndex = df.columns.values #grabs the names of the selected columns
+    npVals = df.values[0].astype(np.int64) #grabs the values of the selected row
+    numScores = 0
+    for i in npVals:
+        numScores += i
+    npSize = np.zeros(npIndex.shape[0],dtype=np.float64) #normalized array for magnitude of results
+    index = 0
+    for i in npVals:
+        npSize[index] = i/numScores
+        index+=1
+    npLocationX = np.zeros(npIndex.shape[0],dtype=np.float64) #gets X position of where serve should be
+    npLocationY = np.zeros(npIndex.shape[0],dtype=np.float64) #gets Y position of where serve should be
+    index = 0
+    for i in npIndex:
+        location = location_map[i]
+        npLocationX[index] = location[0]
+        npLocationY[index] = location[1]
+        index+=1
+    
     df_sample = pd.DataFrame({
-        "serve": ["Serve 1", "Serve 2"],
-        "x": [.2,.8],
-        "y": [.4,.6],
-        "player": ["Player 1", "Player 2"],
-        "team": ["Home","Away"],
-        "size": [.05,.05],
+        "location": npIndex,
+        "x": npLocationX,
+        "y": npLocationY,
+        "size": npSize,
+        "value": npVals
     })
-
-    colour0 = "#009BFF"
-    colour1 = "grey"
-    colour_ball = "red"
-
-    color_discrete_map = {"Home": colour0, "Away": colour1, "Ball": colour_ball}
-
     
     # Plotly Express version
     fig = px.scatter(
         df_sample,
         x="x",
         y="y",
-        color="team",
-        hover_name="player",
+        color="size",
         range_x=[-0.05, 1.05],
         range_y=[-0.05, 1.05],
         size="size",
-        size_max=10,
-        opacity=0.8,
-        color_discrete_map=color_discrete_map,
-        text="serve",
-        hover_data={
-            "x": False,
-            "y": False,
-            "team": False,
-        },
+        color_continuous_scale=px.colors.sequential.Rainbow,
+        hover_data={"value": True},
+        text="value"
     )
 
     # Add corner flags to prevent zoom and pitch distortion
@@ -99,12 +119,12 @@ def match_placement_view():
         )
     )
 
-    pio.templates["custom_dark"] = go.layout.Template()
-    pio.templates["custom_dark"]["layout"]["paper_bgcolor"] = "#282828"
-    pio.templates["custom_dark"]["layout"]["plot_bgcolor"] = "#282828"
+    #pio.templates["custom_dark"] = go.layout.Template()
+    #pio.templates["custom_dark"]["layout"]["paper_bgcolor"] = "#282828"
+    #pio.templates["custom_dark"]["layout"]["plot_bgcolor"] = "#282828"
 
     fig.update_layout(
-        template="custom_dark",
+        #template="custom_dark",
         xaxis=dict(showgrid=False, showticklabels=False),
         # plot_bgcolor='#282828',
         # paper_bgcolor='#282828'
@@ -112,9 +132,9 @@ def match_placement_view():
 
     fig.update_layout(margin=dict(l=20, r=20, b=20, t=20))
 
-    fig.update_layout(
-        legend_orientation="v", transition={"duration": 0, "ordering": "traces first"}
-    )
+    #fig.update_layout(
+    #    legend_orientation="v", transition={"duration": 0, "ordering": "traces first"}
+    #)
 
     # Make sure pitch background image shape doesn't get distorted
     fig.update_yaxes(scaleanchor="x", scaleratio=0.65)
